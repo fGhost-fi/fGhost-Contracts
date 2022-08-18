1758a04857034e8ae8706378be8dceadf9d60cda
0xPhant0m <108011905+fGhostPhant0m@users.noreply.github.com>
2022-08-18 00:54:47 -0400

    Update SpookyVault.sol

--- Contracts/Crematorium/SpookySwap/SpookyVault.sol
+++ Contracts/Crematorium/SpookySwap/SpookyVault.sol
@@ -7,10 +7,10 @@ import "contracts/Support/IERC20Metadata.sol";
 
 interface IMasterChef{
     
-   function deposit(uint pid, uint amount)external{};
-   function withdraw(uint pid, uint amount, address to) external{};
-   function Harvest(address user, uint pid, uint amount) external{};
-   function pendingBOO(uint _pid, address _user) external{} ;
+   function deposit(uint pid, uint amount)external;
+   function withdraw(uint pid, uint amount, address to) external;
+   function Harvest(address user, uint pid, uint amount) external;
+   function pendingBOO(uint _pid, address _user) external;
 }
 
 
@@ -30,34 +30,35 @@ address constant MasterChef = 0x9C9C920E51778c4ABF727b8Bb223e78132F00aA4;
         address memory GhostFarmer,
         address memory Reward
     ) ERC20(name, symbol) ERC4626(asset) { 
-     
+        uint _pid = pid;
         }
-
        
+        
      function totalAssets() public view override returns (uint256) {
         return asset.balanceOf(address(this));
      }
-    function beforeWithdraw (uint256 assets, uint256 shares, uint amount) internal override{
-        SafeIncreaseAllowance(address(asset),address(MasterChef), uint (amount));
-        IMasterChef.Withdraw(address(this),uint(_pid), uint256 (assets), address(this));
-        SafeDecreaseAllowance(address(asset), address(MasterChef), 0);
+    function beforeWithdraw (uint256 assets, uint256 shares) internal override{
+       ERC20.SafeIncreaseAllowance(address(asset),address(MasterChef), uint (assets));
+        IMasterChef.withdraw(uint(pid), uint (assets), address(this));
+        ERC20.safeDecreaseAllowance(address(asset), address(MasterChef), 0);
          beforeWithdrawHookCalledCounter++;
     }
-    function afterDeposit(uint256, uint256) internal override{
-         SafeIncreaseAllowance(asset, address(MasterChef), amount);
-        IMasterChef.Deposit(address(this),uint(_pid), amount, address(this));
-        SafeDecreaseAllowance(asset, address(MasterChef), 0);
+    function afterDeposit(uint256 assets, uint256 shares) internal override{
+         ERC20.safeIncreaseAllowance(asset, address(MasterChef), assets);
+        IMasterChef.deposit(address(this),uint(_pid), assets, address(this));
+        ERC20.safeDecreaseAllowance(asset, address(MasterChef), 0);
         afterDepositHookCalledCounter++;
     }
 
     function Harvest() external{
-     uint amount = IMasterChef.PendingBOO(uint(_pid), address(this));
+     uint amount = IMasterChef.pendingBOO(uint(_pid), address(this));
        {
 
-        IMasterChef.Harvest(address(this), _pid, amount);
-        SafeERC20.safeTransfer(Reward, address(this), GhostFarmer, balanceOf(address(Reward)));
+        IMasterChef.Harvest(address(this),uint(pid), amount);
+        ERC20.safeTransfer(Reward, address(this), address(GhostFarmer), balanceOf(address(Reward)));
     }
 
     }
 }
+
 
