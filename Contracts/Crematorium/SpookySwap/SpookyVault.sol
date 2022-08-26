feaaf9dde9aaf08d55eed026c36e7623193c2a2e
fghostPhant0m <phant0m242.hotmail.com>
2022-08-26 17:44:03 -0400

    updated spookyvault.sol

--- Contracts/Crematorium/SpookySwap/SpookyVault.sol
+++ Contracts/Crematorium/SpookySwap/SpookyVault.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.14;
 import {ERC20} from "Contracts/Support/ERC20.sol";
 import {ERC4626} from "Contracts/Support/ERC4626.sol";
 import "Contracts/Support/IERC20Metadata.sol";
+import "Contracts/Support/SafeERC20.sol";
+import "Contracts/Support/Math/SafeMath.sol";
 
 interface IMasterChef{
     
@@ -15,6 +17,7 @@ interface IMasterChef{
 
 contract SpookyVault is ERC4626{
    using SafeERC20 for IERC20;
+   using SafeMath for uint;
     
 address constant MasterChef = 0x9C9C920E51778c4ABF727b8Bb223e78132F00aA4;
 IMasterChef mc = IMasterChef(MasterChef);
@@ -24,21 +27,24 @@ IMasterChef mc = IMasterChef(MasterChef);
     
       uint public _pid;
       address public _ghostFarmer;
-      IERC20 public _reward;
+      IERC20 public  _reward;
       IERC20 public  _asset;
+      address public _ghostChef;
 
     constructor(
         IERC20Metadata asset,
         string memory name,
         string memory symbol,
-        uint pid,
+        uint  pid,
         address GhostFarmer,
-        IERC20 Reward      
+        IERC20 Reward,      
+        address GhostChef
     ) ERC20(name, symbol) ERC4626(asset) { 
-      pid = _pid;
-      GhostFarmer = _ghostFarmer;
-      Reward = _reward;
+      _pid = pid;
+      _ghostFarmer = GhostFarmer;
+      _reward = Reward;
       _asset = asset;
+      _ghostChef = GhostChef;
         }
 
     function beforeWithdraw (uint256 assets,uint256) internal override{
@@ -47,6 +53,7 @@ IMasterChef mc = IMasterChef(MasterChef);
         SafeERC20.safeDecreaseAllowance(_asset, MasterChef, 0);
          beforeWithdrawHookCalledCounter++;
     }
+
     function afterDeposit(uint256 assets, uint256) internal override{
          SafeERC20.safeApprove(_asset, MasterChef, assets);
         mc.deposit(uint(_pid), uint (assets));
@@ -55,13 +62,16 @@ IMasterChef mc = IMasterChef(MasterChef);
     }
 
     function harvest() external{
-      uint amount = mc.pendingBOO(uint(_pid), address(this));
 
-       mc.harvest(address(this),uint(_pid), amount);
+        SafeERC20.safeApprove(_asset, MasterChef, 20000);
+        mc.withdraw( _pid, 0);
+        SafeERC20.safeDecreaseAllowance(_asset, MasterChef, 0);
        SafeERC20.safeTransfer(_reward, address(_ghostFarmer), balanceOf(address(_reward)));
     }
          
       }
     
+
+
 
 
