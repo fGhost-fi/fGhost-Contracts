4d647cfb0d38295834ba377d7f7c8508991a41d7
fghostPhant0m <phant0m242.hotmail.com>
2022-12-17 23:10:22 -0500

    eternal flame update

--- Contracts/eternalFlame/EternalFlame.sol
+++ Contracts/eternalFlame/EternalFlame.sol
@@ -10,8 +10,40 @@ interface xGhostVault{
         function _rewardLength() external view returns (uint256);
          function pendingRewards(uint256 reward)external returns(uint _pending);
 }
-interface FireBirdRouter{
-     function swap( uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
+
+interface IUniswapV2Router {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    )
+        external
+        returns (
+            uint amountA,
+            uint amountB,
+            uint liquidity
+        );
 }
 
 
@@ -33,9 +65,14 @@ uint internal rewardLength = xGhost._rewardLength();
 IERC20 public _token0;
 IERC20 public _token1;
 xGhostVault xGhost = xGhostVault(Vault);
-address constant fbr = 0xe0C38b2a8D09aAD53f1C67734B9A95E43d5981c0;
-FireBirdRouter fireBird = FireBirdRouter(fbr);
- 
+address constant spookyRouter = 0xF491e7B69E4244ad4002BC14e878a34207E38c29;
+ IUniswapV2Router Spooky =  IUniswapV2Router(spookyRouter);
+address constant private WFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
+address constant private USDC = 0x04068DA6C83AFCFA0e13ba15A6696662335D5B75;
+address constant private FGHST = 0x5C762a454B89C70f96e69890d502d795A639A06A;
+IERC20 private ftm = IERC20(WFTM);
+IERC20 private usdc = IERC20(USDC);
+IERC20 private fGhost = IERC20(FGHST);
  
     event Staked(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount);
@@ -75,16 +112,23 @@ _token1 = Token1;
         function listRewards() internal{
             xGhost.listRewards();
         } 
-    
+       
     function forsake() external{
         xGhost.Harvest();
-        //To-Do:  do swaps with fire bird router then addLiquidity with Spookyswap Router. Deposit LP to xGhost Vault. 
+        //To-Do:  do swaps with fire bird router then addLiquidity with Spookyswap Router. Deposit LP to xGhost Vault.
+        uint256 rewardSplit = _token0.balanceOf(address(this)) / 2;
+        uint256 slippage = rewardSplit/100; 
+        uint256 minAmount = rewardSplit - slippage;
+        _swap(address(_token0), address(_token1), rewardSplit, minAmount);
+        uint256 bounty = _token0.balanceOf(address(this)) / 100;
+        uint256 burn = _token1.balanceOf(address(this)) / 100; 
+        _addLiquidity(address(_token0),address(_token1), _token0.balanceOf(address(this)) - bounty, _token1.balanceOf(address(this)) - burn);
         uint256 addedBalance = stakingToken.balanceOf(address(this)) - totalSupply;
         uint compound = (addedBalance / totalSupply * 100);
         uint256 ntc = totalCompounds/100 * compound + totalCompounds;
         totalCompounds = ntc;
-        _token1.safeTransfer( msg.sender, _token1.balanceOf(address(this)));
-        _token0.burn(address(this), _token0.balanceOf(address(this)));
+        _token0.safeTransfer( msg.sender, _token0.balanceOf(address(this)));
+        _token1.burn(address(this), _token1.balanceOf(address(this)));
     }
     function checkBalance(address user) external view returns (uint256){
         _getCompoundFactor;
@@ -98,9 +142,58 @@ _token1 = Token1;
          if(compoundFactor < 1){compoundFactor = 1;}
          return compoundFactor;
     }
+    function _swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMin) internal returns( uint256 amountOut) {
+          IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenIn).approve(address(Spooky), amountIn);
+
+        address[] memory path;
+        path = new address[](2);
+        path[0] = tokenIn;
+        path[1] = tokenOut;
+
+        uint[] memory amounts = Spooky.swapExactTokensForTokens(
+            amountIn,
+            amountOutMin,
+            path,
+            msg.sender,
+            block.timestamp
+        );
+              return amounts[1];
+    
+
+    }
+    function _addLiquidity( 
+        address _tokenA,
+        address _tokenB,
+        uint _amountA,
+        uint _amountB) internal {
+       if (_token0.allowance(address(this),address(Spooky))>0){ 
+           _token0.approve(address(Spooky),0);
+       }
+         if (_token1.allowance(address(this),address(Spooky))>0){ 
+           _token1.approve(address(Spooky),0);
+       }
+        _token0.approve(address(Spooky), _token0.balanceOf(address(this)));
+        _token1.approve(address(Spooky), _token0.balanceOf(address(this)));
+
+            Spooky.addLiquidity(
+                _tokenA,
+                _tokenB,
+                _amountA,
+                _amountB,
+                1,
+                1,
+                address(this),
+                block.timestamp
+            );
+          _token0.approve(address(Spooky),0);
+            _token1.approve(address(Spooky),0);
+    }
+
     modifier updateBalance {
           _getCompoundFactor;
-        if (compoundFactor > 1){uint oldBalance = balanceOf[msg.sender];
+        if (compoundFactor > 1){
+        uint oldBalance = balanceOf[msg.sender];
         uint newBalance = oldBalance * compoundFactor;
         balanceOf[msg.sender] = newBalance;
         lastUpdate[msg.sender] = totalCompounds;
